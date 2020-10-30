aad1bd12385ef3782104663f8427a493ec50f288
stomerger <63244668+stomerger@users.noreply.github.com>
2020-10-30 13:26:24 -0300

    Silder configurado solo falta clientes y info

--- app/src/main/java/com/example/bancobpm/Home_act.java
+++ app/src/main/java/com/example/bancobpm/Home_act.java
@@ -5,15 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 import java.util.ArrayList;
 
 public class Home_act extends AppCompatActivity {
-
+    private ViewFlipper vf;
+    private  int[] imagenes = {R.drawable.a, R.drawable.b,R.drawable.c};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_act);
+
+        vf = (ViewFlipper) findViewById(R.id.slider);
+
+        for (int i = 0; i < imagenes.length; i++) {
+            flip_imagenes(imagenes[i]);
+        }
+    }
+    //configuracion del slider
+    public  void flip_imagenes( int i){
+        ImageView view = new ImageView(this);
+        view.setBackgroundResource(i);
+        vf.addView(view);
+        vf.setFlipInterval(2800);
+        vf.setAutoStart(true);
+
+        vf.setInAnimation( this, android.R.anim.slide_in_left);
+        vf.setOutAnimation(this, android.R.anim.slide_out_right);
     }
     public void info(View view){
         Intent info = new Intent( this, Info_act.class );
